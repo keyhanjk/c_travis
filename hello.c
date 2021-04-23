1a1746c9669dd06aecf521e5aae979bf0827e8cb
keyhan.jannat.khah@gmail.com <keyhan.jannat.khah@gmail.com>
2021-04-23 11:39:29 +0430

    init

--- hello.c
+++ hello.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 
-static int t = 1;
-
 int main()
 {
-    if (t)
-        printf("on this line\n");
-    else
-        printf("but not here\n");
-
+    printf("this is just a test\n");
     return 0;
 }
